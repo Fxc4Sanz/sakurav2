7389a77c0d55f0eb6638d7c05a2c3de3f418f827
assabiq <82302556+Assabiq31213@users.noreply.github.com>
2021-10-24 22:37:06 +0700

    Add files via upload

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,21 +1,30 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-
-  if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/yqyjPX/`
-  if (!args[0].match(/tiktok/gi)) throw `url salah`
-
-  let res = await fetch(API('hardianto', '/api/download/tiktok', { url: args[0] }, 'apikey'))
-  if (!res.ok) throw eror
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+  let res = await fetch(global.API('xteam', '/dl/tiktok', {
+    url: args[0]
+  }, 'APIKEY'))
+  if (res.status !== 200) throw await res.text()
   let json = await res.json()
-  // if (!json.status) throw json
-  await m.reply(wait)
-  await conn.sendFile(m.chat, json.nowm, '', `${json.caption}\n\nJangan Lupa Follow IG @assbq_k`, m)
-
+  if (!json.status) throw json
+  let url = json.server_1 || json.info[0].videoUrl || ''
+  if (!url) throw 'Gagal mengambil url download'
+  let txt = json.info[0].text
+  for (let hashtag of json.info[0].hashtags) txt = txt.replace(hashtag, '*$&*')
+  await conn.sendFile(m.chat, url, 'tiktok.mp4', `
+▶️ *${json.info[0].playCount} Views*
+💟 *${json.info[0].diggCount} Likes*
+🔁 *${json.info[0].shareCount} Shares*
+💌 *${json.info[0].commentCount} Comments*
+🎵 ${json.info[0].musicMeta.musicName} by ${json.info[0].musicMeta.musicAuthor}
+- *By:* ${json.info[0].authorMeta.nickName} (${json.info[0].authorMeta.name})
+- *Desc:*
+${txt}
+  `.trim(), m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok)$/i
 
-handler.limit = true
+handler.command = /^(tik(tok)?(dl)?)$/i
 
 module.exports = handler
