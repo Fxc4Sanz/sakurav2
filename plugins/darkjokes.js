d69b5384fac4061279a3c23629d91ad9a6939b6c
assabiq <82302556+Assabiq31213@users.noreply.github.com>
2021-10-25 12:53:34 +0700

    Add files via upload

--- plugins/darkjokes.js
+++ plugins/darkjokes.js
@@ -1,8 +1,12 @@
-let handler = async(m, { conn }) => {
-conn.sendFile(m.chat, global.API('xteam', '/asupan/darkjoke', {}, 'a21ec307fc8cafb7'), '', 'drag joles',m)
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ')
+  let res = `https://api.xteam.xyz/randomimage/hentai?apikey=a21ec307fc8cafb7`
+  conn.sendFile(m.chat, res, 'darkjokes.jpg', `Gelap Bangg`, m, false)
 }
 handler.help = ['darkjokes']
 handler.tags = ['image']
 handler.command = /^(dragjoles|darkjokes)$/i
 
 module.exports = handler
+
